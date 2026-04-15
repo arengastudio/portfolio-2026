fe1aa13dd7cf8b5a637700b0783fb2ec8bcdbd1a
juancrluna <juancruzluna234@gmail.com>
2026-04-15 16:57:31 -0300

    chore: migrate to unified @astrojs/vercel adapter import (deprecate /serverless)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://juancruzluna.com',
